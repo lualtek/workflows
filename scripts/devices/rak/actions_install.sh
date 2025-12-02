f127308dea991f4935469add60c6accef5cb097d
LasaleFamine <info@godev.space>
2025-12-02 19:59:24 +0100

    fix: installed lib for arduino workflow

--- scripts/devices/rak/actions_install.sh
+++ scripts/devices/rak/actions_install.sh
@@ -6,20 +6,6 @@ set -e
 
 pip3 install clint pyserial setuptools adafruit-nrfutil
 sudo apt-get update
-sudo apt-get install -y gcc-arm-none-eabi binutils-arm-none-eabi
-
-
-# Make sure to cleanup
-rm -rf ${HOME}/.arduino15/packages/arduino
-# make all our directories we need for files and libraries
-mkdir ${HOME}/.arduino15
-mkdir ${HOME}/.arduino15/packages
-mkdir ${HOME}/Arduino
-mkdir ${HOME}/Arduino/libraries
-
-# # install arduino IDE
-export PATH=$PATH:$GITHUB_WORKSPACE/bin
-curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh -s 0.11.0 2>&1
 arduino-cli config init > /dev/null
 arduino-cli core update-index > /dev/null
 
